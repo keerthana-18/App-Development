ff96385e2bd3841d04677e87c2971593098c3341
keerthana-18 <110590919+keerthana-18@users.noreply.github.com>
2023-09-25 13:12:52 +0530

    Add files via upload

---
+++ Day 9/Data.jsx
@@ -0,0 +1,75 @@
+const DATA =[
+    {
+        id:0,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:1,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:2,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:3,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:4,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:5,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:6,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:7,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:8,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    },
+    {
+        id:9,
+        title:"Apple",
+        price: 100,
+        desc:"Calories: A medium-sized organic apple contains about 95 calories, making it a low-calorie food.Dietary Fiber: Apples are an excellent source of dietary fiber, with around 4 grams of fiber per medium apple. This fiber aids digestion and helps keep you feeling full.Vitamins: Apples are rich in vitamin C, providing about 14% of your daily recommended intake. Vitamin C is essential for a healthy immune system and skinMinerals: Organic apples contain small amounts of essential minerals like potassium and a variety of antioxidants.Antioxidants: Apples are packed with antioxidants, including quercetin, catechin, and chlorogenic acid, which help protect cells from damage caused by free radicals.",
+        img : "https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1"
+    }
+    
+   
+];
+export default DATA;
