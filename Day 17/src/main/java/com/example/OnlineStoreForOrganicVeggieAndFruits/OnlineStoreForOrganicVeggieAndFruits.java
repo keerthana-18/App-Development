74c489e8fe6482f500bb02761ec28979be1ca41a
keerthana-18 <110590919+keerthana-18@users.noreply.github.com>
2023-10-12 11:49:46 +0530

    Add files via upload

---
+++ Day 17/src/main/java/com/example/OnlineStoreForOrganicVeggieAndFruits/OnlineStoreForOrganicVeggieAndFruits.java
@@ -0,0 +1,13 @@
+package com.example.OnlineStoreForOrganicVeggieAndFruits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreForOrganicVeggieAndFruits {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreForOrganicVeggieAndFruits.class, args);
+	}
+
+}
