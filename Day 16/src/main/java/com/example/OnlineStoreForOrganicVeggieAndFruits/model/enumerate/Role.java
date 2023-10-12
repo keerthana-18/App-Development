397caba78aa1bf3d88a8a93074ea310ae164c412
keerthana-18 <110590919+keerthana-18@users.noreply.github.com>
2023-10-12 11:44:22 +0530

    Add files via upload

---
+++ Day 16/src/main/java/com/example/OnlineStoreForOrganicVeggieAndFruits/model/enumerate/Role.java
@@ -0,0 +1,9 @@
+package com.example.OnlineStoreForOrganicVeggieAndFruits.model.enumerate;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
